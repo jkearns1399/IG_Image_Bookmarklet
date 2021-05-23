ed461ed6ae78b125d5673db19bf791448a3ec032
jkearns1399 <79485166+jkearns1399@users.noreply.github.com>
2021-05-23 07:59:38 -0700

    Add script file

---
+++ downloader.js
@@ -0,0 +1,20 @@
+javascript:(function(){
+
+    var img= document.querySelector("img.FFVAD");
+    var body = document.querySelector("body");
+    var dwnldBtn = document.createElement('a');
+
+    dwnldBtn.innerText = "Get Image";
+    dwnldBtn.style = "font-family: BlinkMacSystemFont; z-index: 1; position: absolute; top: 13px; left: 13px; border-radius: 5px; background: #cacaca; border-width: 1px;";
+    dwnldBtn.id = "dwnldbtn";
+    dwnldBtn.setAttribute("href",img.src);
+    //dwnldBtn.setAttribute("download","test.jpg");
+
+    body.prepend(dwnldBtn);
+    dwnldBtn.addEventListener("click", download);
+        
+    function download(){
+        console.log(img.src);
+    }
+})()
+
