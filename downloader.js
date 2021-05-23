d892cb42c3025d53b9d7e4c4ee6e28a71c9ec4f8
jkearns1399 <79485166+jkearns1399@users.noreply.github.com>
2021-05-23 09:36:59 -0700

    Update downloader.js
    Refactor

--- downloader.js
+++ downloader.js
@@ -8,13 +8,6 @@ javascript:(function(){
     dwnldBtn.style = "font-family: BlinkMacSystemFont; z-index: 1; position: absolute; top: 13px; left: 13px; border-radius: 5px; background: #cacaca; border-width: 1px;";
     dwnldBtn.id = "dwnldbtn";
     dwnldBtn.setAttribute("href",img.src);
-    //dwnldBtn.setAttribute("download","test.jpg");
-
-    body.prepend(dwnldBtn);
-    dwnldBtn.addEventListener("click", download);
-        
-    function download(){
-        console.log(img.src);
-    }
+    body.prepend(dwnldBtn); 
 })()
 
